e1981b6553c9b727bc4b79e1014ed15bebe77649
Ivan Logvynenko <n13LogvynenkoIvan13n@gmail.com>
2024-03-05 20:33:49 +0100

    now a bit of changes

--- lib/api/general/requests/client.dart
+++ lib/api/general/requests/client.dart
@@ -1,6 +1,9 @@
 import 'package:tumobile/api/general/requests/session.dart';
 
 abstract class Client {
-  Session? _session;
+  final Session? _session;
 
+  Client.empty() : _session = Session.empty();
+
+  Session get session => _session!;
 }
--- lib/api/general/requests/session.dart
+++ lib/api/general/requests/session.dart
@@ -1,9 +1,13 @@
 import 'package:tumobile/api/general/requests/login_status.dart';
 
 base class Session {
-  LoginStatus _loginStatus = LoginStatus.unstated;
+  LoginStatus _loginStatus = LoginStatus.loggedOut;
   String? _login, _password;
-  
+
+  Session.empty()
+      : _loginStatus = LoginStatus.unstated,
+        _login = "",
+        _password = "";
 
   Session.copy(Session session) {
     _loginStatus = session._loginStatus;
